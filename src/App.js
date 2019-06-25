bdaaa134a08c533aa6e5aab073cfea2a7296a5bd
Felipe Vanegas <felipe021793@gmail.com>
2019-06-24 22:21:18 -0500

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
-import react from 'React';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (<div>Soy un test</div>);
+
+
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
