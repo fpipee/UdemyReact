bca7913eb6653733e0146be64eca50072d772e05
Felipe Vanegas <felipe021793@gmail.com>
2019-06-26 23:37:51 -0500

    :construction: adding a faker component

--- src/index.js
+++ src/index.js
@@ -16,6 +16,19 @@ const App = () => {
                     <div className="Text">NIce blog post</div>
                 </div>
             </div>
+        {/*  */}
+        <div className="comment ">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">SAM</a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="Text">NIce blog post</div>
+                </div>
+            </div>
         </div>
     );
 
