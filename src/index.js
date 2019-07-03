baad3be3ea2dd3eaf1db90bba6b7ce406fbf266f
Felipe Vanegas <felipe021793@gmail.com>
2019-07-02 22:44:14 -0500

    reusable components child

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import AprovalCard from './AprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Robsonn"
-                timeAgo="Today at 4:00 pm"
-                comment={"Piu Avanti"}
-                imageL={faker.image.avatar()}
-            />
+            <AprovalCard>
+                <CommentDetail
+                    author="Robsonn"
+                    timeAgo="Today at 4:00 pm"
+                    comment={"Piu Avanti"}
+                    imageL={faker.image.avatar()}
+                />
+            </AprovalCard>
             <CommentDetail
                 author="Elisa"
                 timeAgo="Today at 2:00 pm"
@@ -22,13 +25,7 @@ const App = () => {
                 timeAgo="Today at 3:00 pm"
                 comment={"Avatar"}
                 imageL={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Today at 5:00 pm"
-                comment={"Lorep Ipsum"}
-                imageL={faker.image.avatar()}
-            />
+            />            
         </div>
     );
 
